72f678ca9e3b7cbc681fa15f2eaa940b7d103e16
Tomasz Pasternak <pstomek@gmail.com>
2023-07-27 21:03:28 +0200

    fix company

--- environment/environment.prod.ts
+++ environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  accountancyUrl: 'https://tomaszwpasternak.github.io/nx-company/accountancy',
-  hrUrl: 'https://tomaszwpasternak.github.io/nx-company/hr',
-  marketingUrl: 'https://tomaszwpasternak.github.io/nx-company/marketing',
-  productionUrl: 'https://tomaszwpasternak.github.io/nx-company/production'
+  accountancyUrl: 'https://tomaszwpasternak.github.io/nx-company/accountancy/#',
+  hrUrl: 'https://tomaszwpasternak.github.io/nx-company/hr/#',
+  marketingUrl: 'https://tomaszwpasternak.github.io/nx-company/marketing/#',
+  productionUrl: 'https://tomaszwpasternak.github.io/nx-company/production/#'
 };
